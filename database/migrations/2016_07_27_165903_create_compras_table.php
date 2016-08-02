9e082f1dc73cd02ef0f965a240acbe5106922c9c
Leandro.Battisti <leandro.battisti@senior.com.br>
2016-08-02 14:53:38 -0300

    commit novo

---
+++ database/migrations/2016_07_27_165903_create_compras_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComprasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compras', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('nome');
+            $table->integer('qtd');
+            $table->boolean('pago');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('compras');
+    }
+}
